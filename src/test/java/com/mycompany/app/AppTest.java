263f2a5a1b99e2117c2176e3ad3ad41909263e35
yogi-17 <131268977+yogi-17@users.noreply.github.com>
2023-04-21 17:30:41 +0530

    added existing test methods 5 times

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -17,4 +17,24 @@ public class AppTest
     {
         assertTrue( true );
     }
+    @Test
+    public void shouldAnswerWithTrue1()
+    {
+        assertTrue( true );
+    }
+    @Test
+    public void shouldAnswerWithTrue2()
+    {
+        assertTrue( true );
+    }
+    @Test
+    public void shouldAnswerWithTrue3()
+    {
+        assertTrue( true );
+    }
+    @Test
+    public void shouldAnswerWithTrue4()
+    {
+        assertTrue( true );
+    }
 }
